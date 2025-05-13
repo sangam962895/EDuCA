29f963f37f7b529698a852aa7e84ba8ccfb60eb5
sangam962895 <sangamkumaragupta119@gmail.com>
2025-05-13 20:20:22 +0530

    LMS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
+import React, { useContext } from 'react'
+import { Routes, Route, useLocation, useMatch } from 'react-router-dom'
+import Navbar from './components/student/Navbar'
+import Home from './pages/student/Home'
+import CourseDetails from './pages/student/CourseDetails'
+import CoursesList from './pages/student/CoursesList'
+import Dashboard from './pages/educator/Dashboard'
+import AddCourse from './pages/educator/AddCourse'
+import MyCourses from './pages/educator/MyCourses'
+import StudentsEnrolled from './pages/educator/StudentsEnrolled'
+import Educator from './pages/educator/Educator'
+import 'quill/dist/quill.snow.css'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
+import Player from './pages/student/Player'
+import MyEnrollments from './pages/student/MyEnrollments'
+import Loading from './components/student/Loading'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const isEducatorRoute = useMatch('/educator/*');
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="text-default min-h-screen bg-white">
+      <ToastContainer />
+      {/* Render Student Navbar only if not on educator routes */}
+      {!isEducatorRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/course-list" element={<CoursesList />} />
+        <Route path="/course-list/:input" element={<CoursesList />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+        <Route path='/educator' element={<Educator />}>
+          <Route path='/educator' element={<Dashboard />} />
+          <Route path='add-course' element={<AddCourse />} />
+          <Route path='my-courses' element={<MyCourses />} />
+          <Route path='student-enrolled' element={<StudentsEnrolled />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
